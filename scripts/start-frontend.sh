d3de364c48c7158ba31e750fd4998f8c4a8d63c0
Russ Allbery <rra@lsst.org>
2023-01-24 12:59:02 -0800

    Use the right command line for init
    Invoke as ivoa-cutout-poc, not as vo-cutouts.

--- scripts/start-frontend.sh
+++ scripts/start-frontend.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Start the vo-cutouts frontend application.  Currently creates the database
-# and then starts the server.  Eventually, this will call Alembic to handle
-# database migrations.
+# Start the ivoa-cutout-poc frontend application.  Currently creates the
+# database and then starts the server.  Eventually, this will call Alembic to
+# handle database migrations.
 
 set -eu
 
-vo-cutouts init
+ivoa-cutout-poc init
 uvicorn vocutouts.main:app --host 0.0.0.0 --port 8080
